0699e862b9552dfc855770024758b4081a6e7529
TheOdd <owenhines07@gmail.com>
2017-11-07 11:40:05 -0600

    Replaced request dependency with got.
    About 10x smaller package dependency and promise-based API.

--- runkit.js
+++ runkit.js
@@ -3,7 +3,7 @@ var gradespeed = require("gradespeed-hisd");
 const username = 'YOUR USERNAME HERE';
 const password = 'YOUR PASSWORD HERE';
 
-gradespeed.fetchRaw(username, password, function(err, returnArr) {
+gradespeed(username, password, function(err, returnArr) {
     if (err) {
         console.log(err);
     } else {
